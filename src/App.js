a9bea8af4ae3988b4525e5de66c626f7d0b92b3a
mahadi.joy <mahadihasanjoy95@gmail.com>
2022-11-03 17:48:41 +0600

    NEW AREA ADDED

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   TileLayer,
   Polygon, Popup
 } from 'react-leaflet';
+import O from "leaflet.utm";
 import Marker from 'react-leaflet-enhanced-marker'
 
 import 'leaflet/dist/leaflet.css';
@@ -16,7 +17,16 @@ import markerImg from './10.jpg'
 const center = [41.1129866497784, -74.04109586871861];
 const position = [41.1129866497784, -74.04109586871861];
 
+
+const convertLatLong = () =>{
+  // require('leaflet')
+  // let item = O.utm({x: 467486.3, y: 4101149.3, zone: 30, band: 'S'});
+  // let cords = item.latLng();
+  // console.log("CORDS::::::::::::::::::::::::::::::::::::: ",cords)
+}
+
 export default function App() {
+  convertLatLong()
   return (
     <MapContainer
       center={center}
@@ -31,7 +41,7 @@ export default function App() {
         statesData.features.map((state) => {
           const coordinates = state.geometry.coordinates[0].map((item) => [item[1], item[0]]);
           // debugger
-          console.log("COORDINATE##### ", state.geometry.coordinates[0][0])
+          console.log("COORDINATE##### ", state.geometry.coordinates[0][2])
           return (
               <Marker position={state.marker} icon={<MarkerStyle/>}>
                 <Popup className='request-popup'>
@@ -65,10 +75,11 @@ export default function App() {
                   pathOptions={{
                     // fillColor: '#FD8D3C',
                     // fillOpacity: 0.7,
+                    fillColor:"none",
                     weight: 2,
                     opacity: 1,
                     dashArray: 3,
-                    color: 'white'
+                    color: 'yellow'
                   }}
                   positions={coordinates}
                   eventHandlers={{
@@ -80,7 +91,7 @@ export default function App() {
                         // fillOpacity: 0.7,
                         weight: 2,
                         opacity: 1,
-                        color: "white",
+                        color: "yellow",
                       })
                     },
                     mouseout: (e) => {
@@ -89,7 +100,7 @@ export default function App() {
                         // fillOpacity: 0.7,
                         weight: 2,
                         dashArray: "3",
-                        color: 'white',
+                        color: 'yellow',
                         // fillColor: '#FD8D3C'
                       });
                     },
--- src/data.js
+++ src/data.js
@@ -5,14 +5,14 @@ export const statesData = {
       type: "Feature",
       id: "1",
       properties: { name: "10 Seabring St", density: 412.3 },
-      img:"./10",
+      img:"./10.jpg",
       details: "Distance: 0.91 miles\n" +
           "Printkey: 57.39-2-50\n" +
           "Sale Date: 03/25/2021\n" +
           "Sq Foot Living Area: 2,328\n" +
           "Building Style : Raised Ranch\n" +
           "Neighborhood: SPRING VALLEY",
-      marker: [41.1129866497784, -74.04109586871861],
+      marker: [41.11296537096311, -74.0409138927495],
       geometry: {
         type: "Polygon",
         coordinates: [
@@ -28,8 +28,8 @@ export const statesData = {
     {
       type: "Feature",
       id: "2",
-      img:'./10',
-      marker: [41.112439, -74.041205],
+      img:'./11.jpg',
+      marker: [41.112622, -74.040951],
       properties: { name: "11 Seabring St", density: 412.3 },
       details: "Distance: 0.89 miles\n" +
           "Printkey: 50.11-3-26\n" +
@@ -45,6 +45,225 @@ export const statesData = {
             [ -74.041365,41.112349],
             [ -74.040854,41.112096],
             [ -74.040951,41.112869],
+          ],
+        ],
+      },
+    },
+    // {
+    //   type: "Feature",
+    //   id: "3",
+    //   img:'./11.jpg',
+    //   marker: [41.112646, -74.040134],
+    //   properties: { name: "12 Seabring St", density: 412.3 },
+    //   details: "Distance: 0.89 miles\n" +
+    //       "Printkey: 50.11-3-26\n" +
+    //       "Sale Date: 06/17/2021\n" +
+    //       "Sq Foot Living Area: 2,076\n" +
+    //       "Building Style : Raised Ranch\n" +
+    //       "Neighborhood: TOWN OUT",
+    //   geometry: {
+    //     type: "Polygon",
+    //     coordinates: [
+    //       [
+    //         [-74.040902,41.113237],
+    //         [-74.040175,41.113208],
+    //         [ -74.039912,41.111584],
+    //         [-74.040381,41.111519],
+    //         [-74.040491,41.111833],
+    //         [-74.040759,41.112041],
+    //       ],
+    //     ],
+    //   },
+    // },
+    {
+      type: "Feature",
+      id: "4",
+      img:'./11.jpg',
+      marker: [41.11235542033378, -74.0393868074395],
+      properties: { name: "13 Seabring St", density: 412.3 },
+      details: "Distance: 0.89 miles\n" +
+          "Printkey: 50.11-3-26\n" +
+          "Sale Date: 06/17/2021\n" +
+          "Sq Foot Living Area: 2,076\n" +
+          "Building Style : Raised Ranch\n" +
+          "Neighborhood: TOWN OUT",
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [-74.040047,41.113213],
+            [-74.039295,41.113196],
+            [-74.039074,41.111661],
+            [-74.039816,41.111607],
+          ],
+        ],
+      },
+    },
+    {
+      type: "Feature",
+      id: "5",
+      img:'./11.jpg',
+      marker: [41.11354875385535, -74.03974118414077],
+      properties: { name: "15 Seabring St", density: 412.3 },
+      details: "Distance: 0.89 miles\n" +
+          "Printkey: 50.11-3-26\n" +
+          "Sale Date: 06/17/2021\n" +
+          "Sq Foot Living Area: 2,076\n" +
+          "Building Style : Raised Ranch\n" +
+          "Neighborhood: TOWN OUT",
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [-74.040311,41.113829],
+            [-74.039516,41.113851],
+            [-74.039511,41.113280],
+            [-74.040276,41.113311],
+          ],
+        ],
+      },
+    },{
+      type: "Feature",
+      id: "6",
+      img:'./11.jpg',
+      marker: [41.113080109656416, -74.04063366345403],
+      properties: { name: "16 Seabring St", density: 412.3 },
+      details: "Distance: 0.89 miles\n" +
+          "Printkey: 50.11-3-26\n" +
+          "Sale Date: 06/17/2021\n" +
+          "Sq Foot Living Area: 2,076\n" +
+          "Building Style : Raised Ranch\n" +
+          "Neighborhood: TOWN OUT",
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [ -74.04088476328067,41.11322674608044],
+            [-74.0405838866283,41.11322232605886],
+            [ -74.04050461319991,41.112909641657076],
+            [-74.04086522553247,41.11290838382917],
+          ],
+        ],
+      },
+    },
+    {
+      type: "Feature",
+      id: "7",
+      img:'./11.jpg',
+      marker: [41.11306247502775, -74.04023263530136],
+      properties: { name: "17 Seabring St", density: 412.3 },
+      details: "Distance: 0.89 miles\n" +
+          "Printkey: 50.11-3-26\n" +
+          "Sale Date: 06/17/2021\n" +
+          "Sq Foot Living Area: 2,076\n" +
+          "Building Style : Raised Ranch\n" +
+          "Neighborhood: TOWN OUT",
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [-74.04052404490841,41.11320155457138],
+            [-74.0404730554829,41.11319631602047],
+            [-74.04047460061702,41.113222508770896],
+            [-74.0401794800092,41.11320504693843],
+            [-74.04010302602674,41.11291091435936],
+            [-74.04047782243727,41.11290444997195],
+          ],
+        ],
+      },
+    },
+    {
+      type: "Feature",
+      id: "8",
+      img:'./11.jpg',
+      marker: [41.11272506488207, -74.04056032367735],
+      properties: { name: "18 Seabring St", density: 412.3 },
+      details: "Distance: 0.89 miles\n" +
+          "Printkey: 50.11-3-26\n" +
+          "Sale Date: 06/17/2021\n" +
+          "Sq Foot Living Area: 2,076\n" +
+          "Building Style : Raised Ranch\n" +
+          "Neighborhood: TOWN OUT",
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [-74.04086522553247,41.11290838382917],
+            [-74.04050461319991,41.112909641657076],
+            [-74.0404479771632,41.112563045971456],
+            [-74.04081040099163,41.112533675638666],
+          ],
+        ],
+      },
+    },
+    {
+      type: "Feature",
+      id: "9",
+      img:'./11.jpg',
+      marker: [41.112721414687186, -74.04018259145306],
+      properties: { name: "19 Seabring St", density: 412.3 },
+      details: "Distance: 0.89 miles\n" +
+          "Printkey: 50.11-3-26\n" +
+          "Sale Date: 06/17/2021\n" +
+          "Sq Foot Living Area: 2,076\n" +
+          "Building Style : Raised Ranch\n" +
+          "Neighborhood: TOWN OUT",
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [-74.0404744121231,41.11282341799107],
+            [-74.04011742406394,41.112851045357225],
+            [-74.04007104797165,41.11257802146407],
+            [-74.04043666414101,41.11256258257903],
+          ],
+        ],
+      },
+    },
+    {
+      type: "Feature",
+      id: "10",
+      img:'./11.jpg',
+      marker: [41.11235028688989, -74.04046825627337],
+      properties: { name: "21 Seabring St", density: 412.3 },
+      details: "Distance: 0.89 miles\n" +
+          "Printkey: 50.11-3-26\n" +
+          "Sale Date: 06/17/2021\n" +
+          "Sq Foot Living Area: 2,076\n" +
+          "Building Style : Raised Ranch\n" +
+          "Neighborhood: TOWN OUT",
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [-74.04081040099163,41.112533675638666],
+            [-74.0404479771632,41.112563045971456],
+            [-74.04040096674512,41.11228994422292],
+            [-74.04076863597614,41.11228135083882],
+          ],
+        ],
+      },
+    },
+    {
+      type: "Feature",
+      id: "1q",
+      img:'./11.jpg',
+      marker: [41.11211634750655, -74.04049055752382],
+      properties: { name: "22 Seabring St", density: 412.3 },
+      details: "Distance: 0.89 miles\n" +
+          "Printkey: 50.11-3-26\n" +
+          "Sale Date: 06/17/2021\n" +
+          "Sq Foot Living Area: 2,076\n" +
+          "Building Style : Raised Ranch\n" +
+          "Neighborhood: TOWN OUT",
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [ -74.04076863597614,41.11228135083882],
+            [-74.04040096674512,41.11228994422292],
+            [-74.04035710253174,41.11200904110134],
+            [ -74.04072493212077,41.11199317831279],
           ],
         ],
       },
